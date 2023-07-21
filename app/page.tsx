694b6fe8d0a30c7883b79aae01efade8ba562d3b
N <nabindashrestha@gmail.com>
2023-07-21 11:05:44 +0545

    home page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,33 @@
 'use client'
 
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import wait from '../components/Image/wait.gif'
 import Image from 'next/image';
 import ProductCard from '@/components/ProductCard';
+import { getAllProducts } from '@/api/api';
+import { Product } from '@/types/types';
 
 export default function Home() {
   
-  const fetchProduct=async()=> {
-    const response = await axios.get('https://fakestoreapi.com/products/');
-      const data = await response.data;
-      return data;
-  }
-  
-  const { isLoading, isError, error, data } = useQuery({
+  const { isLoading, isError, data } = useQuery({
     queryKey: ['products'],
-    queryFn: fetchProduct,
+    queryFn: getAllProducts,
   })
 
   if (isLoading) {
     return <div className="flex justify-center items-center h-screen">
-          Wait for a moment
           <Image src={wait} width={200} height={200} alt="loading" />
         </div>
   }
 
   if (isError) {
-    return <span>Error: {error.message}</span>
+    return <span>Error</span>
   }
 console.log(data)
   return (
     <div className="container mx-auto antialiased bg-gray-200 text-gray-900 font-sans p-6">
-    <div className='flex flex-wrap -mx-4'>
-      {data.map((product) => {
+    <div className='flex flex-wrap items-stretch-mx-4'>
+      {data.map((product: Product) => {
               return <ProductCard product={product} key={product.id} />;
             })}
     </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className= "bg-neutral-200 text-center text-white dark:bg-neutral-600 py-2">
+    <footer className= "bg-neutral-200 text-center text-white dark:bg-neutral-900 py-2">
       Copyright &copy; 2023, All Rights Reserved
     </footer>
   )
